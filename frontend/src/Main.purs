dd88ec8f9e73e43c879002619ee85d0815f2c787
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2021-04-05 10:19:04 -0700

    Fixes bug where timezone is UTC0

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude (Unit, Void, discard, bind, join, const, pure, show, unit, ($), (<>), (<$>), (>>=))
 
+import Effect.Console (log)
 import Data.Array(cons)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Maybe.Trans
@@ -22,6 +23,8 @@ import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 import Web.HTML.Common (ClassName(..))
 import Effect.Now (nowDateTime)
+import Data.DateTime as Date
+import Data.Time.Duration as Time 
 
 import InputField as InputField 
 import WorkoutSelector as WorkoutSelector
@@ -136,8 +139,17 @@ component =
             workout <- MaybeT $ H.request WorkoutSelector.proxy unit WorkoutSelector.GetValue
             reps <- MaybeT $ join <$> H.request InputField.natProxy 0 InputField.GetValue
             weight <- MaybeT $ join <$> H.request InputField.natProxy 1 InputField.GetValue
-            date <- format dateReadFormat <$> (lift $ H.liftEffect nowDateTime)
-            pure $ WorkoutSet { name: workout, reps: natToInt reps, date: date, weight: natToInt weight }
+            date <- format dateReadFormat <$> (MaybeT $ do
+                                                 t <- H.liftEffect nowDateTime
+                                                 pure $ Date.adjust (Time.negateDuration (Time.Hours 7.0)) t
+                                              )
+            lift $ H.liftEffect $ log $ show date
+            pure $ WorkoutSet
+              { name: workout
+              , reps: natToInt reps
+              , date: date
+              , weight: natToInt weight
+              }
       gather >>= case _ of
         Just ws -> do
           postJson ws "/sets"
